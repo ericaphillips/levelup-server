4f0912d9b2ed4a26fad44885490a56873ba430cf
ericaphillips <ericaphill@gmail.com>
2021-02-12 11:34:32 -0600

    finished chapter 9 or so

--- levelup/urls.py
+++ levelup/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
 from rest_framework import routers
-from levelupapi.views import GameTypes
+from levelupapi.views import GameTypes, GamesView, Events
 
-router = routers.DefulatRouter(trailing_slash=False)
+router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypes, 'gametype')
+router.register(r'games', GamesView, 'game')
+router.register(r'events', Events, 'event')
 
 urlpatterns = [
     path('', include(router.urls)),
--- levelupapi/models/games.py
+++ levelupapi/models/games.py
@@ -3,7 +3,8 @@ from django.db.models.fields import CharField
 
 class Games(models.Model):
     title = models.CharField(max_length=100)
-    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
+    maker = models.CharField(max_length=75)
+    gametype = models.ForeignKey("GameType", on_delete=models.CASCADE)
     number_of_players = models.IntegerField()
     gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
-    description = models.CharField(max_length=500)
+    skill_level = models.IntegerField()
